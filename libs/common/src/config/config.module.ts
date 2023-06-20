67d2a37ca78a170c56643b4c62ccaeb158a46dc7
Labeeb Ahmad <labeebahmad201@gmail.com>
2023-06-20 05:11:08 -0400

    feat: added database module, that gets env variable using config module, also included validation for env variable

---
+++ libs/common/src/config/config.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule as NestConfigModule, ConfigService } from '@nestjs/config';
+import * as joi from "joi";
+
+@Module({
+    imports: [
+        NestConfigModule.forRoot({
+            validationSchema: joi.object({
+                MONGODB_URI: joi.string().required()
+            })
+        })
+    ],
+    providers: [ConfigService],
+    exports: [ConfigService]
+})
+export class ConfigModule {}
---
+++ libs/common/src/database/database.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@app/common/config/index';
+
+@Module({
+    imports: [MongooseModule.forRootAsync({
+        imports: [ConfigModule],
+        useFactory: (configService: ConfigService)=>({
+            uri: configService.get('MONGODB_URI')
+        }),
+        inject: [ConfigService], // for use inside factory
+    })],
+})
+export class DatabaseModule {}
